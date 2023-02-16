c06f7b67902bacc256d7b31dfd547188599164a0
AyariAhmed <ayari17@hotmail.fr>
2023-02-16 15:18:09 +0100

    api v1

--- src/main/scala/http/Api.scala
+++ src/main/scala/http/Api.scala
@@ -1,91 +1,73 @@
 package http
 
-//import akka.actor.ActorSystem
-//import akka.http.scaladsl.server.Route
-//import akka.http.scaladsl.Http
-//import akka.http.scaladsl.server.Directives._
-//import akka.stream.Materializer
-//import com.datastax.oss.driver.api.core.{CqlIdentifier, CqlSession}
-//import com.datastax.oss.driver.api.core.cql.ResultSet
-//import spray.json.DefaultJsonProtocol._
-//import spray.json.RootJsonFormat
-//import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-//
-//import java.net.InetSocketAddress
-//import java.util.concurrent.CompletionStage
-//import scala.collection.convert.ImplicitConversions.`iterable AsScalaIterable`
-//import scala.concurrent.{ExecutionContextExecutor, Future}
-//
-//case class ResponseData(total_sales_promo_cat: Int, incremental_lift: Option[Int], promo_lift: Double)
-//
-//object ResponseData {
-//  implicit val ResponseDataFormat: RootJsonFormat[ResponseData] = jsonFormat3(ResponseData.apply)
-//}
-//
-//object Api extends App {
-//  implicit val system: ActorSystem = ActorSystem("Api")
-//  implicit val materializer: Materializer = Materializer(system)
-//  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
-//
-//  val cassandraSession: CqlSession = CqlSession.builder().addContactPoint(new InetSocketAddress("127.0.0.1", 9042)).build()
-//  cassandraSession.execute("USE " + CqlIdentifier.fromCql("challenges")) // set keyspace to challenges
-//
-//  def queryDatabase(promo_cat: String, prod_id: Int): Future[List[ResponseData]] = Future {
-//    val selectStmt = "SELECT total_sales_promo_cat, incremental_lift, promo_lift FROM sales_data WHERE promo_cat = ? and prod_id = ?"
-//    val preparedStatement = cassandraSession.prepare(selectStmt)
-//    val boundStatement = preparedStatement.bind(promo_cat, prod_id)
-//    val resultSetFuture: CompletionStage[ResultSet] = cassandraSession.executeAsync(boundStatement)
-//
-//
-//    resultSetFuture.map(row =>
-//      ResponseData(row.getInt("total_sales_promo_cat"), Option(row.getInt("incremental_lift")), row.getDouble("promo_lift"))
-//    ).toList
-//  }
-//
-//  private val route: Route = {
-//    path("sales") {
-//      get {
-//        parameters("promo_cat", "prod_id") { (promo_cat, prod_id) =>
-//          val dbQueryResult: Future[List[ResponseData]] = queryDatabase(promo_cat, prod_id.toInt)
-//          onSuccess(dbQueryResult) {
-//            complete(dbQueryResult)
-//          }
-//        }
-//      }
-//    }
-//  }
-//
-//  val host = "localhost"
-//  val port = 8080
-//  Http().newServerAt(host, port).bindFlow(route)
-//
-//  println(s"Server online at http://localhost:8080/users/{id}")
-//}
-
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.Materializer
+import com.datastax.oss.driver.api.core.cql.ResultSet
+import com.datastax.oss.driver.api.core.{CqlIdentifier, CqlSession}
+import spray.json.DefaultJsonProtocol._
+import spray.json._
 
-import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import java.net.InetSocketAddress
 import scala.io.StdIn
+import scala.util.{Failure, Success}
+
+case class ResponseData(total_sales_promo_cat: Int, incremental_lift: Option[Int], promo_lift: Double)
+
+object MyJsonProtocol extends DefaultJsonProtocol {
+  implicit val responseDataFormat: RootJsonFormat[ResponseData] = jsonFormat3(ResponseData)
+}
 
 object Api extends App {
-  implicit val system: ActorSystem = ActorSystem("my-system")
+  implicit val system: ActorSystem = ActorSystem("API")
   implicit val materializer: Materializer = Materializer(system)
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+  implicit val responseDataFormat: RootJsonFormat[ResponseData] = MyJsonProtocol.responseDataFormat
 
-  val route: Route = path("hello") {
+  private val cassandraSession: CqlSession = CqlSession.builder()
+    .addContactPoint(new InetSocketAddress("127.0.0.1", 9042))
+    .withLocalDatacenter("datacenter1")
+    .withKeyspace(CqlIdentifier.fromCql("challenge"))
+    .build()
+
+  def queryDatabase(promo_cat: String, prod_id: Int) = Future {
+    val selectStmt = "SELECT total_sales_promo_cat, incremental_lift, promo_lift FROM sales_data WHERE promo_cat = {promo_cat} and prod_id = {prod_id}"
+    val boundStatement = cassandraSession.prepare(selectStmt).bind()
+    boundStatement.setString("promo_cat", promo_cat)
+    boundStatement.setInt("prod_id", prod_id)
+
+    //    val resultSet = cassandraSession.execute(boundStatement)
+
+    cassandraSession.execute(boundStatement)
+  }
+
+  private val route = path("sales") {
     get {
-      complete(StatusCodes.OK, "Hello, world!")
+      parameters("promo_cat", "prod_id") { (promo_cat, prod_id) => {
+        import scala.collection.JavaConverters._
+        val resultSet: Future[ResultSet] = queryDatabase(promo_cat, prod_id.toInt)
+        resultSet.onComplete {
+          case Failure(exception) => complete(StatusCodes.InternalServerError, s"Error occurred while executing the query: ${exception.getMessage}")
+          case Success(result) => {
+            val response = result.iterator.asScala.toStream.map(row =>
+              ResponseData(row.getInt("total_sales_promo_cat"), Option(row.getInt("incremental_lift")), row.getDouble("promo_lift"))
+            ).toList.toJson
+            complete(StatusCodes.OK, "hello")
+          }
+        }
+      }
+      }
     }
   }
 
   val host = "localhost"
   val port = 8080
-  val bindingFuture = Http().newServerAt(host, port).bindFlow(route)
+  private val bindingFuture = Http().newServerAt(host, port).bindFlow(route)
 
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
   StdIn.readLine()
